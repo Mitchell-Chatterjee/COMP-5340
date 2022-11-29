6d968067e7c4ef4931d74a59e1ceac8743a09f4a
Mitchell-Chatterjee <mitchell.chatterjee@gmail.com>
2022-11-28 19:14:40 -0500

    Functioning DCGAN

--- Assignment 4/WGAN.py
+++ Assignment 4/WGAN.py
@@ -1,26 +1,37 @@
+import os
 import numpy as np
+import math
+import sys
+from time import time
+import torchvision.transforms as transforms
+from torchvision.utils import save_image
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torch.autograd import Variable
 import torch.nn as nn
+import torch.nn.functional as F
+import torch
+import torchvision
+import matplotlib.pyplot as plt
+import warnings
 
 
 class Opt(object):
-    """
-    This class defines the global optimization settings.
-    """
     dim = 10
     n_epochs = 200
     batch_size = dim*dim
-    lr = 0.0002
-    b1 = 0.5
-    b2 = 0.999
+    lr = 0.00005
     n_cpu = 1
     latent_dim = 100
     img_size = 28
     channels = 1
+    n_critic = 5
+    clip_value = 0.01
     sample_interval = 400
 
 
 class Generator(nn.Module):
-    def __init__(self, img_shape, opt):
+    def __init__(self, opt, img_shape):
         super(Generator, self).__init__()
 
         def block(in_feat, out_feat, normalize=True):
@@ -30,38 +41,34 @@ class Generator(nn.Module):
             layers.append(nn.LeakyReLU(0.2, inplace=True))
             return layers
 
-        self.opt = opt
         self.img_shape = img_shape
         self.model = nn.Sequential(
-            *block(self.opt.latent_dim, 128, normalize=False),
+            *block(opt.latent_dim, 128, normalize=False),
             *block(128, 256),
             *block(256, 512),
             *block(512, 1024),
-            nn.Linear(1024, int(np.prod(self.img_shape))),
+            nn.Linear(1024, int(np.prod(img_shape))),
             nn.Tanh()
         )
 
     def forward(self, z):
         img = self.model(z)
-        img = img.view(img.size(0), *self.img_shape)
+        img = img.view(img.shape[0], *self.img_shape)
         return img
 
 
 class Discriminator(nn.Module):
-    def __init__(self, img_shape):
+    def __init__(self, opt):
         super(Discriminator, self).__init__()
-        self.img_shape = img_shape
         self.model = nn.Sequential(
-            nn.Linear(int(np.prod(self.img_shape)), 512),
+            nn.Linear(opt.img_size ** 2, 512),
             nn.LeakyReLU(0.2, inplace=True),
             nn.Linear(512, 256),
             nn.LeakyReLU(0.2, inplace=True),
             nn.Linear(256, 1),
-            nn.Sigmoid(),
         )
 
     def forward(self, img):
-        img_flat = img.view(img.size(0), -1)
+        img_flat = img.view(img.shape[0], -1)
         validity = self.model(img_flat)
         return validity
-
